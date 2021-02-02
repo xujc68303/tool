2b9d9d5f22bbe32d891feb0ea82bf69206337482
jiachenxu <943522184@qq.com>
2021-02-02 21:45:06 +0800

    add

---
+++ src/main/java/com/xjc/tool/string/IDesensitizationUtil.java
@@ -0,0 +1,21 @@
+package com.xjc.tool.string;
+
+/**
+ * @Version 1.0
+ * @ClassName IDesensitizationUtil
+ * @Author jiachenXu
+ * @Date 2021/2/2
+ * @Description 脱敏工具类
+ */
+public class IDesensitizationUtil {
+
+    private static String REPLACE_CHAR = "*";
+
+    private static String FORMAT = "(?<=\\w{%d})\\w(?=\\w{%d})";
+
+    public String around(String str, int left, int right){
+        if(str == null ||(str.length()<left+right+1)) return str;
+        return str.replaceAll(String.format(FORMAT,left,right),REPLACE_CHAR);
+    }
+
+}
